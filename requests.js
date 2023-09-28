98053175a775649c755eb79b25ce74f94592e23b
Flint Rose <oliverflintrose@gmail.com>
2023-09-28 11:46:47 -0500

    first tests running api

---
+++ requests.js
@@ -0,0 +1,35 @@
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+const { key } = require("./config.json");
+
+var raw = JSON.stringify({
+  key: key,
+  prompt: "dog",
+  negative_prompt: null,
+  width: "512",
+  height: "512",
+  samples: "1",
+  num_inference_steps: "20",
+  seed: null,
+  guidance_scale: 7.5,
+  safety_checker: "yes",
+  multi_lingual: "no",
+  panorama: "no",
+  self_attention: "no",
+  upscale: "no",
+  embeddings_model: null,
+  webhook: null,
+  track_id: null,
+});
+
+var requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow",
+};
+
+fetch("https://stablediffusionapi.com/api/v3/text2img", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log("error", error));
